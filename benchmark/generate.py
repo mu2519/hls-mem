45635edd683309a31f002301b8e7eafa0700c9d1
Masayuki Usui <usui-masayuki394@g.ecc.u-tokyo.ac.jp>
2022-12-09 11:19:59 +0900

    added a template system for benchmark programs

---
+++ benchmark/generate.py
@@ -0,0 +1,35 @@
+import tomllib
+from jinja2 import Environment, FileSystemLoader
+
+
+def custom_merge(dst: dict, src: dict) -> None:
+    for k in src:
+        if k in dst:
+            if ((isinstance(src[k], str) and isinstance(dst[k], str)) or
+                (isinstance(src[k], list) and isinstance(dst[k], list))):
+                dst[k] = dst[k] + '\n' + src[k]
+            else:
+                raise TypeError
+        else:
+            dst[k] = src[k]
+
+
+loader = FileSystemLoader('.')
+env = Environment(loader=loader)
+template = env.get_template('template.py.jinja')
+
+with open('./cfg/meta.toml', 'rb') as f:
+    meta = tomllib.load(f)
+
+for k, v in meta.items():
+    if not isinstance(v, list):
+        raise TypeError
+    cfg = {}
+    for n in v:
+        if not isinstance(n, str):
+            raise TypeError
+        with open('./cfg/' + n + '.toml', 'rb') as f:
+            custom_merge(cfg, tomllib.load(f))
+    src = template.render(cfg)
+    with open('./src/' + k + '.py', 'w') as f:
+        f.write(src)
