17991ad7373b6bdb654bcdb00ec55add5bc065af
Masayuki Usui <usui-masayuki394@g.ecc.u-tokyo.ac.jp>
2023-01-05 01:58:02 +0900

    added a test script

---
+++ benchmark/test.py
@@ -0,0 +1,19 @@
+import os
+import subprocess
+from pathlib import Path
+
+subprocess.run(['python', 'generate.py'])
+
+os.chdir('./src')
+
+p = Path('.')
+for f in p.glob('*.py'):
+    rslt = subprocess.run(['python', f.name], capture_output=True, text=True)
+    ac = 'AC' in rslt.stdout
+    wa = 'WA' in rslt.stdout
+    if ac and not wa:
+        print(f.stem, ':', 'PASSED')
+    elif not ac and wa:
+        print(f.stem, ':', 'FAILED')
+    else:
+        raise RuntimeError
